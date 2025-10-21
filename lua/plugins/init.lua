2bce5da3bb8726875c3cf70ffba132dfa51e8efa
klaym3r <klaym3r@gmail.com>
2025-10-21 18:36:56 +0300

    nvim-tree show .gitignore and dotfiles

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,4 +24,18 @@ return {
   		},
   	},
   },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependecies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      require("nvim-tree").setup({
+        filters = {
+          dotfiles = false,
+          git_ignored = false,
+        }
+      })
+    end,
+  }
 }
