8b29d1fe133601e6eb4cb2d54442313d6bfcebfa
klaym3r <klaym3r@gmail.com>
2025-11-25 11:40:20 +0300

    latest

--- init.lua
+++ init.lua
@@ -11,7 +11,6 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
 
 -- load plugins
 require("lazy").setup({
@@ -44,3 +43,56 @@ end)
 
 vim.env.JAVA_HOME = "/usr/lib/jvm/java-17-openjdk"
 vim.env.PATH = vim.env.JAVA_HOME .. "/bin:" .. vim.env.PATH
+
+-- функция перезагрузки темы для NvChad
+local function reload_theme()
+
+  local file_path = os.getenv("HOME") .. "/.config/hypr/theme_state"
+  local theme_file = io.open(file_path, "r")
+
+  if theme_file then
+    local mode = theme_file:read("*all"):gsub("%s+", "")
+    theme_file:close()
+
+    local new_theme = ""
+    local new_bg = ""
+
+    if mode == "dark" then
+      new_theme = "catppuccin"
+      new_bg = "dark"
+    else
+      new_theme = "gruvbox_light"
+      new_bg = "light"
+    end
+
+    local nvconfig = require("nvconfig")
+
+    if nvconfig.base46.theme == new_theme then
+      return
+    end
+
+    nvconfig.base46.theme = new_theme
+
+    require("plenary.reload").reload_module("base46")
+
+    local base46 = require("base46")
+    base46.compile()
+
+    vim.o.background = new_bg
+    base46.load_all_highlights()
+  end
+end
+
+vim.api.nvim_create_autocmd("Signal", {
+  pattern = "SIGUSR1",
+  callback = function()
+    vim.schedule(reload_theme)
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    reload_theme()
+  end,
+})
--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,8 +1,27 @@
 ---@type ChadrcConfig
 local M = {}
 
+local function get_current_theme()
+  local file_path = os.getenv("HOME") .. "/.config/hypr/theme_state"
+  local file = io.open(file_path, "r")
+
+  if file then
+    local mode = file:read("*all"):gsub("%s+", "")
+    file:close()
+    if mode == "light" then
+      return "catppuccin"
+    end
+  end
+
+  return "catppuccin"
+end
+
 M.base46 = {
-  theme = "catppuccin"
+  theme = get_current_theme(),
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
 M.ui = {
